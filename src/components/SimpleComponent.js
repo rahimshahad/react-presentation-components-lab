43347d89fcf3b80f5143437a7cbf15b3a55d68df
rahimshahad <rahim.shahad2@gmail.com>
2021-11-09 22:50:12 -0500

    commit tree 8eb72c5729355cb753d54ca6da93adee73a73080

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+export default class SimpleComponent extends Component{
+    constructor(){
+        super()
+        this.state ={
+            mood : 'happy'
+        }
+    }
+
+    handleClick = () =>{
+        this.setState({
+            mood : "sad"
+        })
+    }
+
+    render(){
+        return <div onClick={this.handleClick}>
+            {this.state.mood}
+        </div>
+    }
+}
